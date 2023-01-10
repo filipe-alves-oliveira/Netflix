87989aa97df5dbf66b8e33461322dfa02b9de5a7
ROL - Rendimento Online <filipe_alvesoliveira@hotmail.com>
2023-01-09 23:02:29 -0300

    conect API

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "aec6bf3fce87d09cebb60f4a5dbf48f5"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let_movies_http = "https://api.themoviedb.org/3/discover/movie?"
