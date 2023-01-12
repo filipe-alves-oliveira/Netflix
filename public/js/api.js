8cbfae04be300872a24160ff036b0cfcd95a7a2f
ROL - Rendimento Online <filipe_alvesoliveira@hotmail.com>
2023-01-11 21:39:40 -0300

    conect API

--- public/js/api.js
+++ public/js/api.js
@@ -4,4 +4,4 @@ let img_url = "https://image.tmdb.org/t/p/w500"
 
 let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
 
-let_movies_http = "https://api.themoviedb.org/3/discover/movie?"
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
---
+++ public/js/home.js
@@ -0,0 +1,41 @@
+const main = document.querySelector(".main")
+fetch(genres_list_http + new URLSearchParams({
+    api_key: api_key
+}))
+.then(res => res.json())
+.then( data => {
+    
+    data.genres.forEach(item => {
+        fetchMoviesListByGenres(item.id, item.name)
+    });
+})
+
+const fetchMoviesListByGenres = (id, genres) => {
+    fetch(movie_genres_http + new URLSearchParams({
+        api_key: api_key,
+        with_genres: id,
+        page: Math.floor(Math.random() *3) +1
+    }))
+    .then(res => res.json())
+    .then(data => {
+        makeCategoryElement(`${genres}_movies`, data.results)
+    })
+    .catch(err => console.log(err))
+}
+
+const makeCategoryElement = (category, data) => {
+    main.innerHTML += `
+    <div class="movie-list">
+
+            <button class="pre-btn">
+                <img src="img/prev.png" alt="previous button">
+            </button>
+
+            <h1 class="movie-category">${category}</h1>
+            <div class="movie-container" id="${category}">
+            </div>
+            <button class="next-btn">
+                <img src="img/next.png" alt="previous button">
+            </button>
+        </div>`
+}
